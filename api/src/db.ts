7a111793f09b4d4291243e98da6758fbad71dbc9
eirikhanasand <eirik.hanasand@gmail.com>
2024-05-24 04:22:29 +0200

    Updated paths

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,6 +1,6 @@
 import admin from 'firebase-admin'
 
-const serviceAccount = require('../secrets/.secret.json')
+const serviceAccount = require('../secrets/.secret2.json')
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
