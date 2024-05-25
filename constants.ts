04238705b0ca9a1c7a9637a61345a47b0efebb22
eirikhanasand <eirik.hanasand@gmail.com>
2024-05-25 14:31:06 +0200

    Updated constants to reflect prod env

--- constants.ts
+++ constants.ts
@@ -1,2 +1,5 @@
 export const TEST_API = 'http://localhost:8080/api'
-export const API = TEST_API //process.env.API_URL || 'http://10.212.137.12:8080/api'
+export const API = process.env.API_URL || 'http://10.212.137.12:8080/api'
+
+// For local testing
+// export const API = TEST_API
