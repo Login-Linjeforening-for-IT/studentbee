0ab4d685957ab35b412e3a30d12bf06fb78ae714
eirikhanasand <eirik.hanasand@gmail.com>
2024-06-02 01:56:59 +0200

    Removed semicolons

--- api/src/flow.ts
+++ api/src/flow.ts
@@ -1,52 +1,53 @@
-import { createClient } from 'redis';
+import { createClient } from 'redis'
 
 // Creates and configure Redis client
 const redisClient = createClient({
   url: 'redis://127.0.0.1:6379'
-});
+})
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
 (async () => {
-  try {
-    await redisClient.connect();
-    console.log('Redis client connected');
-  } catch (err) {
-    console.error('Could not connect to Redis', err);
-  }
-})();
+    try {
+        await redisClient.connect()
+        console.log('Redis client connected')
+    } catch (err) {
+        console.error('Could not connect to Redis', err)
+    }
+})()
 
 // Cache TTL (time to live) in seconds
-const CACHE_TTL = 3600;
+const CACHE_TTL = 3600
 
 // Wrapper for cache function, first checks cache for data, if not found, fetches data from source
 export default async function cache(
-  cacheKey: string,
-  fetchFunction: () => Promise<any>,
-  ttl: number = CACHE_TTL
+    cacheKey: string,
+    fetchFunction: () => Promise<any>,
+    ttl: number = CACHE_TTL
 ): Promise<any> {
-  try {
-    const cachedData = await redisClient.get(cacheKey);
+    try {
+        const cachedData = await redisClient.get(cacheKey)
 
-    // Returns cache hit if possible
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    } else {
-      // If cache miss, fetches data from the source
-      const data = await fetchFunction();
-      // Store the fetched data in cache with TTL
-      await redisClient.set(cacheKey, JSON.stringify(data), {
-        EX: ttl,
-      });
-      return data;
+        // Returns cache hit if possible
+        if (cachedData) {
+            return JSON.parse(cachedData)
+        } else {
+            // If cache miss, fetches data from the source
+            const data = await fetchFunction()
+            // Store the fetched data in cache with TTL
+            await redisClient.set(cacheKey, JSON.stringify(data), {
+                EX: ttl,
+            })
+
+            return data
+        }
+    } catch (error) {
+        console.error('Cache error:', error)
+        throw error
     }
-  } catch (error) {
-    console.error('Cache error:', error);
-    throw error;
-  }
 }
 
 // Properly close the Redis client when the application exits
 process.on('exit', () => {
-  redisClient.quit();
-});
+    redisClient.quit()
+})
