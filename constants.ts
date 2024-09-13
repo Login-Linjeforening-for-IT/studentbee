85e4a77a761476934b258b06ad2991b89fe64e95
eirikhanasand <eirik.hanasand@gmail.com>
2024-09-13 18:24:28 +0200

    Fixed API issues for local env

--- constants.ts
+++ constants.ts
@@ -5,13 +5,13 @@ dotenv.config()
 const { API_URL, BROWSER_API_URL } = process.env
 
 // Checks that all necessary environment variables are defined
-if (!API_URL || !BROWSER_API_URL) {
-    throw new Error('Missing API_URL or / and BROWSER_API_URL (frontend).')
-}
+// if ((!API_URL || !BROWSER_API_URL)) {
+//     throw new Error('Missing API_URL or / and BROWSER_API_URL (frontend).')
+// }
 
 // export const TEST_API = 'http://localhost:8080/api'
 export const API = API_URL || 'http://localhost:8080/api'
-export const BROWSER_API = BROWSER_API_URL || 'https://exam-api.login.no/api'
+export const BROWSER_API = BROWSER_API_URL || 'http://localhost:8080/api'
 
 // For local testing
 // export const API = TEST_API
