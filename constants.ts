3eb927252271dc6b782912f76fea24148ae1e9c8
eirikhanasand <eirik.hanasand@gmail.com>
2024-10-19 17:01:18 +0200

    testing variables

--- constants.ts
+++ constants.ts
@@ -10,8 +10,8 @@ const { API_URL, BROWSER_API_URL } = process.env
 // }
 
 // export const TEST_API = 'http://localhost:8080/api'
-export const API = API_URL || 'https://dev-exam-api.login.no/api'
-export const BROWSER_API = BROWSER_API_URL || 'https://dev-exam-api.login.no/api'
+export const API = API_URL || 'https://exam-api.login.no/api'
+export const BROWSER_API = BROWSER_API_URL || 'https://exam-api.login.no/api'
 
 // For local testing
 // export const API = TEST_API
