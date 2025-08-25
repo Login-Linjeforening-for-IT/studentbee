6568404a7f99c28e89e584af484ebcfeaebf4020
0herm <0herm@protonmail.com>
2025-08-25 20:11:03 +0200

    version tag

--- constants.ts
+++ constants.ts
@@ -1,6 +1,17 @@
+import { version } from './package.json'
+
 const { API_URL, NEXT_PUBLIC_BROWSER_API_URL } = process.env
 
 export const API = API_URL || 'https://exam-api.login.no/api'
 export const BROWSER_API = NEXT_PUBLIC_BROWSER_API_URL || 'https://exam-api.login.no/api'
 // export const API = API_URL || 'http://localhost:8080/api'
 // export const BROWSER_API = NEXT_PUBLIC_BROWSER_API_URL || 'http://localhost:8080/api'
+
+const config = {
+    url: {
+        GITLAB_URL: 'https://gitlab.login.no',
+    },
+    version
+}
+
+export default config
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getCourses } from '@/utils/fetch'
 import Link from 'next/link'
+import config from '@config'
 
 // Main component of the program, holds the main page and the user can navigate
 // to different pages from here
@@ -18,7 +19,7 @@ export default async function Home() {
 
     return (
         <div className='relative h-full rounded-xl overflow-auto noscroll flex justify-center items-center'>
-            <div className='flex flex-col items-center gap-2 py-3 px-4 bg-darker rounded-xl h-[calc(100vh-8rem)] xs:h-120 w-[calc(100vw-1rem)] xs:w-96'>
+            <div className='flex flex-col items-center gap-2 py-3 px-4 bg-darker rounded-xl h-[calc(100vh-12rem)] xs:h-120 w-[calc(100vw-1rem)] xs:w-96'>
                 <h1 className='text-center font-semibold py-2'>Select course</h1>
                 <div className='flex flex-col w-full h-full rounded-xl p-2 overflow-auto noscroll gap-2'>
                     <div className='grid gap-2'>
@@ -33,6 +34,15 @@ export default async function Home() {
                 </div>
                 <Link href='/add/course' className='bg-darker hover:bg-normal w-fit px-4 py-2 grid place-items-center rounded-xl text-2xs md:text-lg'>Add course</Link>
             </div>
+            {typeof config.version !== 'undefined' ? (
+                <Link
+                    className='absolute right-2 bottom-2 bg-[rgba(200,200,200,0.1)] px-[0.6rem] py-[0.4rem] rounded-md text-white tracking-[0.05em] font-semibold'
+                    target='_blank'
+                    href={`${config.url.GITLAB_URL}/tekkom/web/beehive/exam.login.no/-/tags/${config.version}`}
+                >
+                    v{config.version}
+                </Link>
+            ) : null}
         </div>
     )
 }
