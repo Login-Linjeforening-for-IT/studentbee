15a6cbffaeb81fc1cbdfdafeb2e4abf0d517ed5b
0herm <0herm@protonmail.com>
2025-12-02 12:38:18 +0100

    redirect

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,15 @@
 import config from '@config'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { LoginPage } from 'uibee/components'
 
+
 export default async function Page() {
+    const accessToken = (await cookies()).get('access_token')?.value
+    if (accessToken) {
+        redirect('/')
+    }
+
     return (
         <LoginPage
             title='StudentBee'
