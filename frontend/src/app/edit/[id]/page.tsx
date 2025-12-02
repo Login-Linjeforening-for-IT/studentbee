0f4287c15704865cdb0b723c2900f307c2209be2
0herm <0herm@protonmail.com>
2025-12-02 16:53:30 +0100

    center

--- frontend/src/app/edit/[id]/page.tsx
+++ frontend/src/app/edit/[id]/page.tsx
@@ -11,12 +11,14 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 
     if (typeof course === 'string') {
         return (
-            <div className={`
-                bg-login-100/10 backdrop-blur-md border border-login-100/20
-                rounded-2xl shadow-lg p-8 px-100 w-full max-w-md text-center
-                flex flex-col items-center gap-4'
-            `}>
-                <h1>Course not found.</h1>
+            <div className='w-full h-full flex items-center justify-center'>
+                <div className={`
+                    bg-login-100/10 backdrop-blur-md border border-login-100/20
+                    rounded-2xl shadow-lg p-8 px-100 w-full max-w-md text-center
+                    flex flex-col items-center gap-4'
+                `}>
+                    <h1>Course not found.</h1>
+                </div>
             </div>
         )
     }
