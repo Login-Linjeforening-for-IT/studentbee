5249c73890d4a3b94b011b2a59f70b11549d4536
eirikhanasand <eirik.hanasand@gmail.com>
2025-12-02 19:15:33 +0100

    Fixed bug

--- frontend/src/components/comments/voteColor.tsx
+++ frontend/src/components/comments/voteColor.tsx
@@ -1,6 +1,6 @@
 export default function voteColor(direction: 'up' | 'down', votes: Vote[], username: string, clientVote?: number) {
 
-    if (direction === 'up') {
+    if (Array.isArray(votes) && votes.length && direction === 'up') {
         if (votes.some(vote => vote.username === username && vote.vote) && clientVote === 0 || clientVote === 1) {
             return 'text-green-400/50'
         }
@@ -8,7 +8,7 @@ export default function voteColor(direction: 'up' | 'down', votes: Vote[], usern
         return 'text-login-50 hover:text-green-400/50'
     }
 
-    if (votes.some(vote => vote.username === username && !vote.vote) && clientVote === 0 || clientVote === -1) {
+    if (Array.isArray(votes) && votes.length && votes.some(vote => vote.username === username && !vote.vote) && clientVote === 0 || clientVote === -1) {
         return 'text-red-400/50'
     }
 
