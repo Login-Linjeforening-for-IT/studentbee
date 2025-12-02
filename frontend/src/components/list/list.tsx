aeaecf3effb270ae372958acd82b77eacdd433dd
0herm <0herm@protonmail.com>
2025-12-02 17:22:07 +0100

    wider select box

--- frontend/src/components/list/list.tsx
+++ frontend/src/components/list/list.tsx
@@ -23,19 +23,17 @@ export default async function SelectCourseList({ isEdit }: { isEdit?: boolean })
 
     return (
         <div className='relative h-full rounded-lg overflow-auto noscroll flex justify-center items-center'>
-            <div className='bg-login-100/10 backdrop-blur-md border border-login-100/20 rounded-2xl shadow-lg py-3 px-4 w-full max-w-lg text-center flex flex-col items-center gap-2 h-[calc(100vh-20rem)] xs:h-120 xs:w-96'>
+            <div className='bg-login-100/10 backdrop-blur-md border border-login-100/20 rounded-2xl shadow-lg py-3 px-4 w-full max-w-2xl text-center flex flex-col items-center gap-2 h-[calc(100vh-20rem)] xs:h-120 xs:w-96'>
                 <h1 className='text-center font-semibold py-2'>Select course</h1>
                 <div className='flex flex-col w-full h-full rounded-md p-2 overflow-auto noscroll gap-2'>
                     <div className='grid gap-2'>
                         {courses.map((course) => <Link
                             href={`${isEdit ? '/edit' : '/course'}/${course.courseCode}`}
                             key={course.id}
-                            className='text-2xs md:text-base bg-login-300/10 hover:bg-login-300/20 w-full rounded-md p-1 px-2 md:p-2 h-7 md:h-auto flex justify-between items-center'
+                            className='text-2xs md:text-base bg-login-300/10 hover:bg-login-300/20 w-full rounded-md p-1 px-2 md:p-2 h-7 md:h-auto flex justify-between items-center overflow-hidden'
                         >
-                            <div className='flex gap-1'>
-                                <h1>{course.courseCode}</h1>
-                                <h1>-</h1>
-                                <h1>{course.name}</h1>
+                            <div className='flex gap-1 whitespace-nowrap min-w-0 flex-1'>
+                                <h1 className='truncate'>{course.courseCode} - {course.name}</h1>
                             </div>
                             <h1 className='text-login-100/30'>({course.cardCount} cards)</h1>
                         </Link>)}
